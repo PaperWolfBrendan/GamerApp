edcd8c11acb0151a214387dfae08dd4f60575763
PaperWolfBrendan <brendangregan@gmail.com>
2024-07-10 21:28:05 -0400

    Added Nav Bar

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Nav from './Nav'
+import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <Nav />
+  </React.StrictMode>,
+)
